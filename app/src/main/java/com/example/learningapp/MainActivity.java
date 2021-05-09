d6ad9e74a2d76fdfeede24eed5fec4e23b1516c1
sajanasr <saja.nasr.98@gmail.com>
2021-05-09 11:42:57 +0300

    initial push

--- app/src/main/java/com/example/learningapp/MainActivity.java
+++ app/src/main/java/com/example/learningapp/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.learningapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView) this.findViewById(R.id.tvcontiunemain);
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext() ,AboutApp.class);
+                startActivity(intent);
+
+            }
+
+        });
+
     }
 }
+
+
+
---
+++ app/src/main/java/com/example/learningapp/MathWorld.java
@@ -0,0 +1,4 @@
+package com.example.learningapp;
+
+public class MathWorld {
+}
